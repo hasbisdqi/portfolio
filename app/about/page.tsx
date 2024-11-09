2caf0090938d012a02cbfa90883b6f9b0aa2b702
hasbisdqi <muhhasbiassidiqi18@gmail.com>
2024-11-09 15:55:27 +0700

    Enhance About page with structured layout, content, and styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+
+    <main className='mt-24 px-4 mx-auto max-w-screen-xl'>
+      <h1 className='text-4xl font-bold text-center'>
+        About
+      </h1>
+      <p className='text-center text-sm text-muted-foreground mt-4 mb-8'>This is a detailed post about various topics of interest. Stay tuned for more updates and insights!</p>
+
+      <div className="flex gap-4">
+        <img className='' src="https://placehold.co/300x300" alt="" />
+        <p className='text-justify'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, laudantium ducimus? Ducimus voluptatem numquam laudantium alias id, quas nihil molestiae nesciunt tempore maiores quasi impedit explicabo ullam inventore voluptates optio sint nobis et distinctio! Tenetur laborum alias odit ad, ea quae aut quaerat? Voluptas, maxime veritatis error architecto iste nulla numquam aliquam natus minima quam dolore illo quos assumenda sed modi. Nisi tempora sunt soluta alias eius aperiam consectetur deleniti expedita incidunt, fugit porro inventore autem corporis quibusdam quia, dolor enim velit dolorum, molestias suscipit natus? Mollitia, facere dolore reprehenderit eos cupiditate quaerat, ipsam porro quam quod impedit exercitationem debitis autem quis atque, aperiam vero quasi recusandae ut! Provident doloremque, voluptatum eaque similique et unde consequuntur vel recusandae illo ratione ut? Dolore debitis quibusdam culpa aliquam atque optio cupiditate, deserunt consequuntur assumenda natus! Ex placeat officiis molestiae ducimus quis! Quae doloremque aliquid modi ad, debitis ipsum veritatis quod corporis repellendus molestiae. Ab quibusdam eius ipsam dicta adipisci voluptatum magni, vitae asperiores vero quos veniam, dolore doloribus voluptas ratione illo incidunt! Corrupti vitae tempora totam culpa facilis accusamus libero, officiis placeat odio, nostrum excepturi atque ratione nesciunt? Dolor, quos tenetur. Aliquam, magni repudiandae hic neque dicta mollitia ipsa pariatur voluptas totam?
+        </p>
+      </div>
+    </main>
   )
 }
